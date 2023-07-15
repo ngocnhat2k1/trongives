6ea98ebf1d95e6d1732f30b847a76d9fbdab634e
ngocnhat2k1 <1951120116@sv.ut.edu.vn>
2023-07-15 20:13:07 +0700

    lan cuoi

--- src/app/(main)/component/ProfitWithdrawal/index.js
+++ src/app/(main)/component/ProfitWithdrawal/index.js
@@ -6,7 +6,7 @@ import React from 'react'
 const ProfitWithdrawal = () => {
 
     return (
-        <div className='h-[700px] lg:h[974px] w-[110%] bg-cover  bg-no-repeat' style={{ backgroundImage: "url('/Profitwithdrawal.png')" }}>
+        <div className='h-[700px] lg:h-[974px] w-[110%] bg-cover  bg-no-repeat' style={{ backgroundImage: "url('/Profitwithdrawal.png')" }}>
             <div className='h-[400px] lg:h-[545px] pt-8'>
                 <h3 className='text-primary font-bold text-2xl text-center'>Profit withdrawal</h3>
                 <div className='p-0 h-[350px] overflow-hidden'>
